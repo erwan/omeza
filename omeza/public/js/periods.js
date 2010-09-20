a419911a5b51ce74370b13d1dd4d5a12775fb487
Erwan Loisant <eloisant@gmail.com>
2010-09-20 17:26:30 +0200

    jQuery powa

--- omeza/public/js/periods.js
+++ omeza/public/js/periods.js
@@ -1,20 +1,5 @@
-goog.provide('omeza.periods');
+Periods = {};
 
-goog.require('goog.dom');
-goog.require('goog.i18n.DateTimeFormat');
-goog.require('goog.i18n.DateTimeParse');
-goog.require('goog.ui.InputDatePicker');
-
-periods = {};
-
-periods.init = function() {
-  var PATTERN = "yyyy'-'MM'-'dd";
-  var formatter = new goog.i18n.DateTimeFormat(PATTERN);
-  var parser = new goog.i18n.DateTimeParse(PATTERN);
- 
-  var idp1 = new goog.ui.InputDatePicker(formatter, parser);
-  idp1.decorate(goog.dom.getElement('date-field'));
+Periods.init = function() {
+    $("#date-field").datepicker({ dateFormat: 'yy-mm-dd'});
 };
-
-// Ensures the symbol will be visible after compiler renaming.
-goog.exportSymbol('omeza.periods', periods);
