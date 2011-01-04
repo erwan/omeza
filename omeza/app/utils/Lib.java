6eefb1cd4c3aef7c73e1734ae53b2991d1baa996
Erwan Loisant <eloisant@gmail.com>
2011-01-04 22:30:16 +0100

    Minor fixes

--- omeza/app/utils/Lib.java
+++ omeza/app/utils/Lib.java
@@ -7,7 +7,6 @@ public class Lib {
     static final long ONE_HOUR = 60 * 60 * 1000L;
 
     public static long daysDifference(Date d1, Date d2) {
-        return ( (d2.getTime() - d1.getTime() + ONE_HOUR) / 
-                (ONE_HOUR * 24));
+        return ( (d2.getTime() - d1.getTime() + ONE_HOUR) / (ONE_HOUR * 24));
     }
 }
